709d6086e82c5cc141503b13f3e7775f794025f0
aavaaranam <aavaaranam@gmail.com>
2025-10-12 15:25:19 -0700

    Added jasmine scene

--- jasmine.gd
+++ jasmine.gd
@@ -3,13 +3,16 @@ extends Area2D
 @onready var jasmine_plant: AnimatedSprite2D = $JasminePlant
 
 var playerNear = false
+
 func _process(delta: float) -> void:
+	jasmine_plant.play("picking")
+
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("Attack") and playerNear:
+		#jasmine_plant.play("idle")
+
+func _on_body_entered(body: Node2D) -> void:
+	print("body entered")
 	jasmine_plant.play("idle")
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Attack") and playerNear:
-		jasmine_plant.play("picking")
-
-#func _on_body_entered(body: Node2D) -> void:
-	#if (body.is_in_group("player")):
-		#playerNear = true
+	if body.is_in_group("player"):
+		playerNear = true
